2c26f57c7ead3373b1e1e22c3b6e6d6666936676
aliynajjar <ali.y.alnajjar@gmail.com>
2023-03-11 14:57:29 +0200

    Added homepage and styling

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,12 @@ $.ajax({
   dataType:'json',
   success:function(data){
     console.log(data);
+    let news_append = "";
+    const news_cards = document.querySelector(".news_cards")
+    $.each(data, function(index, item){
+      news_append += `<div class="card"><div class="news_card"><div class= "news_title"> ${item.title} </div> <div class= "news_date">${item.date}</div> <div class= "news_description">${item.description}</div> <div class="news_source"><a href="${item.source}" target="_blank">للمزيد</a></div></div></div>`;
+      
+    });
+    $(".news_cards").html(news_append);
   }
 })
